e5fd3f7c471cd5d19fefe58639656883d9c7a7e5
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2021-06-12 12:02:59 +0530

    completed the payment portal

--- Razorpay.js
+++ Razorpay.js
@@ -1,25 +1,35 @@
 
 
+      /* Start client-defined Callback Handler Functions */
+      function onOpenHandler () {
+        alert('Payments Modal is Opened');
+      }
 
-var num=document.getElementById("money").value;
+      function onCloseHandler () {
+        alert('Payments Modal is Closed');
+      }
 
-var options = {
-    "key": "rzp_test_bvqnCBn720tlsa	", // Enter the Key ID generated from the Dashboard
-    "amount": "num*100", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-    "currency": "INR",
-    "name": "Acme Corp",
-    "description": "Test Transaction",
-    "image": "https://example.com/your_logo",
-    "order_id": "order_Ef80WJDPBmAeNt", //Pass the `id` obtained in the previous step
-    "account_id": "acc_Ef7ArAsdU5t0XL",
-    "handler": function (response){
-        alert(response.razorpay_payment_id);
-        alert(response.razorpay_order_id);
-        alert(response.razorpay_signature)
-    }
-};
-var rzp1 = new Razorpay(options);
-document.getElementById('rzp-button1').onclick = function(e){
-    rzp1.open();
-    e.preventDefault();
-}
+      function onPaymentSuccessHandler (response) {
+        alert('Payment Success');
+        console.log('Payment Success Response', response);
+      }
+
+      function onPaymentFailureHandler (response) {
+        alert('Payment Failure');
+        console.log('Payment Failure Response', response);
+      }
+      /* End client-defined Callback Handler Functions */
+
+      /* Configuring Handlers */
+      Instamojo.configure({
+        handlers: {
+          onOpen: onOpenHandler,
+          onClose: onCloseHandler,
+          onSuccess: onPaymentSuccessHandler,
+          onFailure: onPaymentFailureHandler
+        }
+      });
+      
+      function onButtonClick() {
+        Instamojo.open('http://test.instamojo.com/@vikasbhalla36');
+      }
