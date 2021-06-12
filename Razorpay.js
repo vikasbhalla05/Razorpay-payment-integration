346de58ee9248cdf08936de4037ffec73257f568
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2021-06-12 08:37:39 +0530

    payment links added

--- Razorpay.js
+++ Razorpay.js
@@ -1,2 +1,25 @@
 
 
+
+var num=document.getElementById("money").nodeValue;
+
+var options = {
+    "key": "rzp_test_bvqnCBn720tlsa	", // Enter the Key ID generated from the Dashboard
+    "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "currency": "INR",
+    "name": "Acme Corp",
+    "description": "Test Transaction",
+    "image": "https://example.com/your_logo",
+    "order_id": "order_Ef80WJDPBmAeNt", //Pass the `id` obtained in the previous step
+    "account_id": "acc_Ef7ArAsdU5t0XL",
+    "handler": function (response){
+        alert(response.razorpay_payment_id);
+        alert(response.razorpay_order_id);
+        alert(response.razorpay_signature)
+    }
+};
+var rzp1 = new Razorpay(options);
+document.getElementById('rzp-button1').onclick = function(e){
+    rzp1.open();
+    e.preventDefault();
+}
